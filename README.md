da86b143aeee78e1e60450f21b741077d54cec22
mattcmerritt <mattcmerritt@gmail.com>
2022-11-14 19:31:37 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Serial-Monitor
+## About
+A simple serial monitoring program that can be used as an alternative to the Arduino serial monitor. Allows for the selection of COM ports on Windows machines.
+
+## Dependencies
+This project utilized the pySerial module for connecting to and monitoring ports. You can install this using either of the following:
+
+`pip install pyserial`
+
+`conda install pyserial`
+
+Alternatively, you can use conda to load all the dependencies from `requirements.txt` using the following command (replace `<env>` with the environment name):
+
+`conda create --name <env> --file requirements.txt`
+
+Note to self: To use conda in powershell, I had to add the following directories to my PATH
+- `C:\Users\Scarfy\anaconda3` - My conda installation
+- `C:\Users\Scarfy\anaconda3\Scripts` - All the applications conda uses
+Then, on opening powershell, run the following:
+`conda init powershell`
+Exit powershell, and you should be able to activate and deactivate conda environments in a new powershell terminal.
+To disable conda, run
+`conda init --reverse powershell`
---
+++ serial_monitor.py
@@ -0,0 +1,66 @@
+import serial
+import serial.tools.list_ports
+from tkinter import *
+from tkinter import ttk
+from threading import Thread
+
+def check_serial_output(port):
+    while not type(port) and port != None and port.isOpen():
+        if port.in_waiting > 0:
+            line = port.readline().decode("utf-8").strip()
+            output_console.insert(END, line)
+
+def attempt_port_selection(index, value, op):
+    if option_select.get() in port_names:
+        try:
+            print("Attempting connection...")
+            port = serial.Serial(option_select.get())
+            print("Connection completed!")
+            warning_label.grid_forget()
+            missing_label.grid_forget()
+            connected_label.grid(column=0, columnspan=2, row=1)
+            return port    
+        except:
+            connected_label.grid_forget()
+            missing_label.grid_forget()
+            warning_label.grid(column=0, columnspan=2, row=1)
+            return None
+    else:
+        connected_label.grid_forget()
+        missing_label.grid_forget()
+        warning_label.grid(column=0, columnspan=2, row=1)
+        return None    
+
+if __name__ == "__main__":
+    # fetching all active ports
+    port_names = list(map(lambda port: port.name, serial.tools.list_ports.comports()))
+
+    # creating a window to display the port selection
+    root = Tk()
+    root.title("Serial Monitor")
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+
+    label = ttk.Label(frm, text="Please select a port:")
+    label.grid(column=0, row=0)
+
+    # creating a listener to see when the contents of the combobox change
+    selected = StringVar()
+    active_port = selected.trace_add('write', attempt_port_selection)
+
+    option_select = ttk.Combobox(frm, textvar=selected, values=port_names)
+    option_select.grid(column=1, row=0)
+
+    missing_label = ttk.Label(frm, text="No port selected.", foreground='#f00')
+    warning_label = ttk.Label(frm, text="Port could not be found!", foreground='#f00')
+    connected_label = ttk.Label(frm, text="Connected to port.")
+    missing_label.grid(column=0, columnspan=2, row=1)
+
+    output_console = Text(frm, height=20, state=DISABLED, xscrollcommand=True, yscrollcommand=True, padx=10, pady=10)
+    output_console.grid(column=0, columnspan=2, row=2)
+
+    output_thread = Thread(target=check_serial_output(active_port))
+    output_thread.start()
+    output_thread.join()
+
+    root.mainloop()
